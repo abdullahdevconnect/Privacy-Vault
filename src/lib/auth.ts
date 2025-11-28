c94571cfd0eedd672e7f31fcf97dd4f756259b45
Muhammad Abdullah <muhammadabdullah.dev.info@gmail.com>
2025-11-29 04:46:25 +0500

    10: Payments-Setup

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,8 @@
+import { checkout, polar, portal } from "@polar-sh/better-auth";
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import db from "@/lib/db";
+import { polarClient } from "./polar";
 
 
 export const auth = betterAuth({
@@ -12,4 +14,23 @@ export const auth = betterAuth({
     enabled: true,
     autoSignIn: true,
   },
+  plugins: [
+    polar({
+      client: polarClient,
+      createCustomerOnSignUp: true,
+      use: [
+        checkout({
+          products: [
+            {
+              productId: "9e8398d2-f0dc-4927-bbfc-0eb17db96ce5",
+              slug: "pro",
+            },
+          ],
+          successUrl: process.env.POLAR_SUCCESS_URL,
+          authenticatedUsersOnly: true,
+        }),
+        portal({}),
+      ],
+    }),
+  ],
 });
